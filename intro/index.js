0dd3498e2bf1bf199abb62b30c8aa8a17a781567
SidW111 <siddhantwainngade@gmail.com>
2025-09-14 14:57:06 +0530

    first commit

---
+++ intro/index.js
@@ -0,0 +1,35 @@
+//1. Scene
+const scene = new THREE.Scene();
+
+//creating a object
+const geometry = new THREE.BoxGeometry(2, 2, 2);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+
+//2. Object
+const box = new THREE.Mesh(geometry, material);
+
+//adding the object to the scene.
+scene.add(box);
+
+//specifying aspect ratio for the camera
+const size = {
+    width:window.innerWidth,
+    height:window.innerHeight 
+}
+
+//3. Camera
+const camera = new THREE.PerspectiveCamera(75,size.width/size.height)
+camera.position.z = 4;
+
+
+//adding the camera to the scene
+scene.add(camera)
+
+//canvas for rendering
+const target = document.querySelector('.wbgl')
+
+//4. Rendering
+const renderer= new THREE.WebGLRenderer({canvas:target})
+
+renderer.setSize(size.width,size.height)
+renderer.render(scene,camera)
