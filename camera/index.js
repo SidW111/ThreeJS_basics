5ca238fba1619bc0e9e4b89363d814a7e166c6c9
SidW111 <siddhantwainngade@gmail.com>
2025-09-14 16:30:31 +0530

    camera

---
+++ camera/index.js
@@ -0,0 +1,54 @@
+//1. Scene
+const scene = new THREE.Scene();
+
+//creating a object
+const geometry = new THREE.BoxGeometry(1, 1, 1,5,5,5);
+const material = new THREE.MeshBasicMaterial({ color: "red" });
+
+//2. Object
+const box = new THREE.Mesh(geometry, material);
+
+//adding the object to the scene.
+scene.add(box);
+
+//specifying aspect ratio for the camera
+const size = {
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
+
+const aspectRatio = size.width / size.height;
+//3. Camera
+// const camera = new THREE.PerspectiveCamera(75, aspectRatio);
+const camera = new THREE.OrthographicCamera(
+  -1 * aspectRatio,
+  1 * aspectRatio,
+  1,
+  -1,
+  0.1,
+  100
+);
+camera.position.z = 2;
+camera.position.x = 2;
+camera.position.y = 2
+camera.lookAt(box.position)
+
+//adding the camera to the scene
+scene.add(camera);
+
+//canvas for rendering
+const target = document.querySelector(".wbgl");
+
+//4. Rendering
+const renderer = new THREE.WebGLRenderer({ canvas: target });
+
+renderer.setSize(size.width, size.height);
+
+let clock = new THREE.Clock();
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+box.rotation.y = elapsedTime;
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
