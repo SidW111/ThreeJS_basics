7a814ce97539fcd4f3f604a59a2e4d5e68e1e495
SidW111 <siddhantwainngade@gmail.com>
2025-09-20 16:30:59 +0530

    lil-gui debug-Ui

--- practice/src/index.js
+++ practice/src/index.js
@@ -1,34 +1,98 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import GUI from "lil-gui";
+import gsap from "gsap";
+
+const gui = new GUI({
+  
+  title:"Debug UI",
+  closeFolders:true
+});
+const guiProps = {};
+const scrollables = gui.addFolder("Scrollable");
+const checks = gui.addFolder("checkbox");
+const color = gui.addFolder("color");
+color.close();
+scrollables.close();
+checks.close();
+window.addEventListener('keydown',(e)=>{
+if(e.key === 'h') {
+  gui.show(gui._hidden)
+}
+})
+
+
 
 const size = {
   width: window.innerWidth,
   height: window.innerHeight,
 };
-console.log(size.width);
 const aspectRatio = size.width / size.height;
 
 const target = document.querySelector(".webgl");
 
-//fullscreen 
-window.addEventListener('dblclick',()=>{
-  if(!document.fullscreenElement){
-    target.requestFullscreen()
-  }else{
-    document.exitFullscreen()
+//fullscreen
+window.addEventListener("dblclick", () => {
+  if (!document.fullscreenElement) {
+    target.requestFullscreen();
+  } else {
+    document.exitFullscreen();
   }
-})
+});
 
 //scene
 const scene = new THREE.Scene();
 
 //object
-const geometry = new THREE.SphereGeometry(10,3,3,3,3,);
-const material = new THREE.MeshBasicMaterial({ color: "red", wireframe: true });
+guiProps.color = "#ff0000";
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({
+  color: guiProps.color,
+  wireframe: true,
+});
 
 const box = new THREE.Mesh(geometry, material);
 
 scene.add(box);
+scrollables.add(box.position, "x").min(-3).max(3).name("position X");
+scrollables.add(box.position, "y").min(-3).max(3).name("position Y");
+scrollables.add(box.position, "z").min(-3).max(3).name("position Z");
+
+checks.add(material, "wireframe");
+checks.add(box, "visible");
+
+color.addColor(guiProps, "color").onChange(() => {
+  material.color.set(guiProps.color);
+});
+
+guiProps.spin = () => {
+  gsap.to(box.rotation, {
+    delay: 1,
+    y: box.rotation.y + Math.PI * 2,
+    duration: 3,
+  });
+};
+
+gui.add(guiProps, "spin");
+
+guiProps.segment = 2;
+
+scrollables
+  .add(guiProps, "segment")
+  .onChange(() => {
+    box.geometry.dispose();
+    box.geometry = new THREE.BoxGeometry(
+      1,
+      1,
+      1,
+      guiProps.segment,
+      guiProps.segment,
+      guiProps.segment
+    );
+  })
+  .min(1)
+  .max(15)
+  .step(1);
 
 //camera
 const camera = new THREE.PerspectiveCamera(75, aspectRatio);
