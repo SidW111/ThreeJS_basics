2a54160a1a7f9a34e5ef328ecb4f7e5d6a9ad6a7
SidW111 <siddhantwainngade@gmail.com>
2025-09-30 22:00:08 +0530

    shadows

--- shadows/src/script.js
+++ shadows/src/script.js
@@ -10,29 +10,85 @@ const canvas = document.querySelector(".webgl");
 //scene
 const scene = new THREE.Scene();
 
+//light
+const ambientLight = new THREE.AmbientLight();
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+directionalLight.position.set(2, 2, -1);
+
+// directionalLight.shadow.camera.near = 6;
+// directionalLight.shadow.camera.far = 6;
+directionalLight.shadow.camera.top = 2;
+directionalLight.shadow.camera.right = 2;
+directionalLight.shadow.camera.left = -2;
+directionalLight.shadow.camera.bottom = -2;
+
+directionalLight.castShadow = true;
+scene.add(directionalLight);
+
+directionalLight.shadow.mapSize.width = 1024;
+directionalLight.shadow.mapSize.height = 1024;
+
+const spotLight = new THREE.SpotLight(0xffffff, 3.6, 10, Math.PI * 0.3);
+scene.add(spotLight);
+
+// const directionalLightHelper = new THREE.CameraHelper(
+//   directionalLight.shadow.camera
+// );
+// scene.add(directionalLightHelper);
+
 //object
 // const geometry = new THREE.SphereGeometry(1, 32, 32);
-const geometry = new THREE.SphereGeometry(1, 32, 32);
-const material = new THREE.MeshDepthMaterial({});
+const geometry = new THREE.SphereGeometry(0.9, 32, 32);
+const material = new THREE.MeshStandardMaterial();
+material.roughness = 0.7;
+// material.metalness = 0.9;
 
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
+const sphere = new THREE.Mesh(geometry, material);
+scene.add(sphere);
+sphere.castShadow = true;
 
-const planeGeometry = new THREE.PlaneGeometry(5,5)
-const plane = new THREE.Mesh(planeGeometry,material)
-plane.position.y = -1.5
-plane.rotation.x = -Math.PI * 0.5
+const planeGeometry = new THREE.PlaneGeometry(5, 5);
+const plane = new THREE.Mesh(planeGeometry, material);
+plane.position.y = -1;
+plane.rotation.x = -Math.PI * 0.5;
 
+scene.add(plane);
+plane.receiveShadow = true;
 
-scene.add(plane)
+const loader = new THREE.TextureLoader();
+const bakedshadow = loader.load("/simpleShadow.jpg");
+
+const sphereShadow = new THREE.Mesh(
+  new THREE.PlaneGeometry(2, 2),
+  new THREE.MeshBasicMaterial({
+    color: 0x00000,
+    alphaMap: bakedshadow,
+    transparent: true,
+  })
+);
+sphereShadow.rotation.x = -Math.PI * 0.5;
+sphereShadow.position.y = plane.position.y + 0.01;
+scene.add(sphereShadow);
+
 //camera
-const camera = new THREE.PerspectiveCamera(75, size.width / size.height);
+const camera = new THREE.PerspectiveCamera(
+  75,
+  size.width / size.height,
+  0.1,
+  100
+);
+camera.position.x = 1;
+camera.position.y = 1;
 camera.position.z = 4;
+
 scene.add(camera);
 
 //renderer
 const renderer = new THREE.WebGLRenderer({ canvas: canvas });
 renderer.setSize(size.width, size.height);
+renderer.shadowMap.enabled = true;
 renderer.render(scene, camera);
 
 const controls = new OrbitControls(camera, canvas);
@@ -46,13 +102,24 @@ window.addEventListener("resize", () => {
   camera.updateProjectionMatrix();
 
   renderer.setSize(size.width, size.height);
-  renderer.render(scene, camera);
+  // renderer.render(scene, camera);
   renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 });
 
+const clock = new THREE.Clock();
 const tick = () => {
+  const elapsedTime = clock.getElapsedTime();
+
+  sphere.position.x = Math.sin(elapsedTime) * 1.5;
+  sphere.position.z = Math.cos(elapsedTime) * 1.5;
+  sphere.position.y = Math.abs(Math.sin(elapsedTime * 4));
+
+  sphereShadow.position.x = sphere.position.x;
+  sphereShadow.position.z = sphere.position.z;
+  sphereShadow.material.opacity = (1 - sphere.position.y) * 0.5;
+
   controls.update();
   renderer.render(scene, camera);
-  requestAnimationFrame(tick);
+  window.requestAnimationFrame(tick);
 };
 tick();
